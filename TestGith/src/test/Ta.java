4074f49544ccd93d1a7eaa6d17564bad23811eab
Administrator <sada@sfs.com>
2017-07-12 15:32:24 +0800

    sum(7,8)

--- TestGith/src/test/Ta.java
+++ TestGith/src/test/Ta.java
@@ -3,9 +3,12 @@ package test;
 public class Ta {
 
 	String ss="aa";
+	public static int sum(int x,int y){
+		return x+y;
+	}
 	public static void main(String[] args) {
 		
-		
+		sum(7,8);
 	}
 
 }
